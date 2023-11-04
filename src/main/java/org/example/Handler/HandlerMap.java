9de3bf95ecf3669c33e69c24d3263c2c4374584b
hyunw9 <rkdgusdnr32@naver.com>
2023-11-05 00:50:34 +0900

    feat: handlerMap

---
+++ src/main/java/org/example/Handler/HandlerMap.java
@@ -0,0 +1,11 @@
+package org.example.Handler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HandlerMap {
+
+  private static final Map<String, HandlerFunction> handlerMap = new HashMap<>();
+
+
+}
