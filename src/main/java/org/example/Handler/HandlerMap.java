1371bb66e9aa79549e6772831f10dcf3f83911ef
hyunw9 <rkdgusdnr32@naver.com>
2023-11-12 00:41:56 +0900

    feat: handlerMap

--- src/main/java/org/example/Handler/HandlerMap.java
+++ src/main/java/org/example/Handler/HandlerMap.java
@@ -2,10 +2,36 @@ package org.example.Handler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class HandlerMap {
 
-  private static final Map<String, HandlerFunction> handlerMap = new HashMap<>();
+  private final Map<String, Consumer<String>>  handlerMap ;
 
+  public HandlerMap(Map<String, Consumer<String>> handlerMap) {
+    this.handlerMap = handlerMap;
+  }
 
+  public static HandlerMap addInitialFuncAndCreateMap(){
+    Map<String, Consumer<String>>  handlerMap = new HashMap<>();
+    handlerMap.put("CSRooms",HandlerFunction::on_cs_rooms);
+    handlerMap.put("CSJoinRoom",HandlerFunction::on_cs_join);
+    handlerMap.put("CSLeaveRoom",HandlerFunction::on_cs_leave);
+    handlerMap.put("CSChat",HandlerFunction::on_cs_chat);
+    handlerMap.put("CSLeave",HandlerFunction::on_cs_leave);
+    handlerMap.put("CSShutdown",HandlerFunction::on_cs_shutdown);
+    return new HandlerMap(handlerMap);
+  }
+
+  public void put(String name, Consumer<String> function){
+    handlerMap.put(name, function);
+  }
+
+  public Consumer<String> getFunction(String name) {
+    return handlerMap.get(name);
+  }
+
+  public Map<String, Consumer<String>> getHandlerMap() {
+    return handlerMap;
+  }
 }
