ad99b66d6a7ee69881509ad32672d7d79981b9c7
hyunw9 <rkdgusdnr32@naver.com>
2023-11-12 00:41:45 +0900

    feat: main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,10 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
+  public static void main(String[] args) throws IOException, InterruptedException {
+    ServerController.startChatServer();
 
-  public static void main(String[] args) {
-    int n =0;
-    Thread thread = new Thread(new threadTest(n));
-    Thread thread2 = new Thread(new threadTest(n));
-    thread.start();
-    thread2.start();
   }
-}
-
-class threadTest implements Runnable {
-
-  private final int param;
-
-  public threadTest(int param) {
-    this.param = param;
-  }
-
-  @Override
-  public void run() {
-    for (int i = 0; i < 1000; i++) {
-      System.out.println(param + i);
-    }
-  }
-
 }
