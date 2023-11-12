97c2063d3ce943a2a85075a9a1af6910fdf9e9e6
hyunw9 <rkdgusdnr32@naver.com>
2023-11-13 00:52:28 +0900

    feat: 필요 함수 추가

--- src/main/java/org/example/Handler/HandlerMap.java
+++ src/main/java/org/example/Handler/HandlerMap.java
@@ -3,24 +3,30 @@ package org.example.Handler;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+import org.example.Model.ChatRoomManager;
 
 public class HandlerMap {
 
   private final Map<String, Consumer<String>>  handlerMap ;
+  private HandlerFunction handlerFunctions;
 
-  public HandlerMap(Map<String, Consumer<String>> handlerMap) {
+
+  public HandlerMap(Map<String, Consumer<String>> handlerMap,HandlerFunction functions) {
     this.handlerMap = handlerMap;
+    this.handlerFunctions = functions;
   }
 
-  public static HandlerMap addInitialFuncAndCreateMap(){
+  public static HandlerMap addInitialFuncAndCreateMap(ChatRoomManager chatRoomManager){
     Map<String, Consumer<String>>  handlerMap = new HashMap<>();
+    handlerMap.put("CSName",HandlerFunction::on_cs_name);
     handlerMap.put("CSRooms",HandlerFunction::on_cs_rooms);
+    handlerMap.put("CSCreateRoom",HandlerFunction::on_cs_create);
     handlerMap.put("CSJoinRoom",HandlerFunction::on_cs_join);
     handlerMap.put("CSLeaveRoom",HandlerFunction::on_cs_leave);
     handlerMap.put("CSChat",HandlerFunction::on_cs_chat);
-    handlerMap.put("CSLeave",HandlerFunction::on_cs_leave);
     handlerMap.put("CSShutdown",HandlerFunction::on_cs_shutdown);
-    return new HandlerMap(handlerMap);
+    HandlerFunction handlerFunction = new HandlerFunction(chatRoomManager);
+    return new HandlerMap(handlerMap,handlerFunction);
   }
 
   public void put(String name, Consumer<String> function){
