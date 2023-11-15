856ec277d02aedf3a4afdf121f5bf75e6ea1d710
hyunw9 <rkdgusdnr32@naver.com>
2023-11-16 01:50:48 +0900

    feat: User 모델

--- src/main/java/org/example/Model/User.java
+++ src/main/java/org/example/Model/User.java
@@ -1,18 +1,30 @@
 package org.example.Model;
 
+import java.nio.channels.SocketChannel;
+
 public class User {
 
   private String name;
+  private SocketChannel socketChannel;
 
-  public User(String userName)  {
-    this.name = userName;
+  public User(String name, SocketChannel socketChannel) {
+    this.name = name;
+    this.socketChannel = socketChannel;
   }
 
   public String getName() {
     return name;
   }
 
-  public void setName(String userName) {
-    this.name = userName;
+  public SocketChannel getSocketChannel() {
+    return socketChannel;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setSocketChannel(SocketChannel socketChannel) {
+    this.socketChannel = socketChannel;
   }
 }
