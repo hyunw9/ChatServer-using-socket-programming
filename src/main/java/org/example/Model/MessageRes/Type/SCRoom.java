95434d38e73b2f0f84a50b4a54098ab35c6a3775
hyunw9 <rkdgusdnr32@naver.com>
2023-11-18 15:07:02 +0900

    feat: SCRoom Response 객체 추가

---
+++ src/main/java/org/example/Model/MessageRes/Type/SCRoom.java
@@ -0,0 +1,28 @@
+package org.example.Model.MessageRes.Type;
+
+import java.util.List;
+
+public class SCRoom {
+
+    private String roomId;
+    private String title;
+    private List<String> members;
+
+    public SCRoom(String roomId, String title, List<String> members) {
+        this.roomId = roomId;
+        this.title = title;
+        this.members = members;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<String> getMembers() {
+        return members;
+    }
+}
