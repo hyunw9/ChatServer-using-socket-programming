43b84302b537c36d623aefa3c321906312661923
hyunw9 <rkdgusdnr32@naver.com>
2023-11-18 15:06:32 +0900

    feat: SCChat Response 추가

---
+++ src/main/java/org/example/Model/MessageRes/Type/SCChatRes.java
@@ -0,0 +1,35 @@
+package org.example.Model.MessageRes.Type;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.nio.ByteBuffer;
+import org.example.Model.MessageRes.Serializer.MessageSerializer;
+
+public class SCChatRes implements MessageRes {
+
+  @JsonProperty
+  private String type;
+
+  @JsonProperty
+  private String text;
+
+  @JsonProperty
+  private String member;
+
+  public SCChatRes(String text, String member) {
+    this.type = "SCChat";
+    this.text = text;
+    this.member = member;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public String getMember() {
+    return member;
+  }
