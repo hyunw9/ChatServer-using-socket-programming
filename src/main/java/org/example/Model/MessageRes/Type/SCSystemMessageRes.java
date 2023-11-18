d52cdb7a94ff8d626ca5b89c4bedfb04fc3887ab
hyunw9 <rkdgusdnr32@naver.com>
2023-11-18 15:07:44 +0900

    feat: SCSystemMessage Response 객체 추가

---
+++ src/main/java/org/example/Model/MessageRes/Type/SCSystemMessageRes.java
@@ -0,0 +1,27 @@
+package org.example.Model.MessageRes.Type;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.nio.ByteBuffer;
+import org.example.Model.MessageRes.Serializer.MessageSerializer;
+
+public class SCSystemMessageRes implements MessageRes {
+
+  @JsonProperty
+  private String type;
+
+  @JsonProperty
+  private String text;
+
+  public SCSystemMessageRes(String text) {
+    this.type = "SCSystemMessage";
+    this.text = text;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public String getText() {
+    return text;
+  }
+
