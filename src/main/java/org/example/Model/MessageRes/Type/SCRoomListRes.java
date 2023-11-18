0214a0f9eacdb982f3a9c1adaaca64ce17f2d7c9
hyunw9 <rkdgusdnr32@naver.com>
2023-11-18 15:07:20 +0900

    feat: SCRoomList Response 객체 추가

---
+++ src/main/java/org/example/Model/MessageRes/Type/SCRoomListRes.java
@@ -0,0 +1,33 @@
+package org.example.Model.MessageRes.Type;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.nio.ByteBuffer;
+import java.util.List;
+import org.example.Model.MessageRes.Serializer.MessageSerializer;
+
+public class SCRoomListRes implements MessageRes {
+
+  @JsonProperty
+  private String type;
+
+  @JsonProperty
+  private List<SCRoom> rooms;
+
+  public SCRoomListRes(List<SCRoom> rooms) {
+    this.type = "SCRoomsResult";
+    this.rooms = rooms;
+  }
+
+  public List<SCRoom> getSCRoomResList(){
+    return rooms;
+  }
+
+  public void setSCRoomResList(List<SCRoom> rooms){
+    this.rooms = rooms;
+  }
+
+  @Override
+  public String getType() {
+    return type;
+  }
+
